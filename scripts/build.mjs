08689259a4419a4b824ebbf70299d4f9343182fd
Jackson <jrobe3221@gmail.com>
2021-07-20 12:24:38 -0500

    Updated webpack layout and configuration

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -7,14 +7,19 @@ await $`yarn install --silent`
 const options = process.argv.slice(3);
 let parameters = {
   webpack: false,
-  typescript: true
+  typescript: true,
+  mode: "production"
 }
 
 options.map((opt, index) => {
   if (['--webpack', '-wp'].includes(opt)) parameters.webpack = true;
+  if (['--dev', '-d'].includes(opt)) parameters.mode = 'development'
 });
 
 if (parameters.typescript) await $`tsc`;
-if (parameters.webpack) await $` webpack --mode development --config webpack.config.js`
+if (parameters.webpack) {
+  await $` webpack --mode development -o dist --output-filename eventra.js --config webpack/webpack.config.js --no-stats`
+  await $` webpack --mode production -o dist --output-filename eventra.min.js --config webpack/webpack.config.js --no-stats`
+}
 
 console.log('Done!');
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,9 +3,10 @@ const webpack = require('webpack')
 
 module.exports = {
   entry: './lib/index.js',
+  
   output: {
     path: './webpack',
-    filename: 'bundle.js',
+    filename: 'eventra.js',
     libraryTarget: "var",
     library: 'Eventra',
     path: path.resolve(__dirname, 'webpack'),
